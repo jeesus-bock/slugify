5b87cc8cc831851545aa73c118a3e02611a5ba8d
Jeesus Bock <90532510+jeesus-bock@users.noreply.github.com>
2024-06-19 05:59:11 +0300

    Update slugify.ts

--- slugify.ts
+++ slugify.ts
@@ -1,18 +1,31 @@
 export const slugify = (str: string) => {
   if(!str) return '';
-  let retStr = str.toLowerCase();
-  const repChars = [' ', '+', '&'];
-  for(const repChar of repchars) {
-    retStr = retStr.replace(repChar, '-');
-  }
+  let repStr = str.toLowerCase();
+  
+  // Replace whitespaces with dash.
+  repStr = repStr.replace(/\s/g, '-');
+  
+  // Replace the individual characters assumed to be in the input.
+  repStr = repStr.replaceAll('ö', 'o');
+  repStr = repStr.replaceAll('ä', 'a');
+  
+  // Replace the rest of non-english non-numeric characters.
+  repStr = repStr.replace(/[^a-z^0-9^-]+/g, "-");
+  
+  // Remove consecutive dashes.
   let lastChar = '';
-  let tmpStr = '';
-  for (const letter of inputData) {
+  let retStr = '';
+  for (const letter of repStr) {
     if(letter == '-') {
-      if(lastChar != '-') { tmpStr += '-' }
+      if(lastChar != '-') { retStr += '-' }
     } else {
-      tmpStr += letter;
+      retStr += letter;
     }
     lastChar = letter;
+  }
   
+  // Remove trailing dash if exists.
+  if(retStr.endsWith('-')) retStr = retStr.slice(0, -1);
+  
+  return retStr;
 }
